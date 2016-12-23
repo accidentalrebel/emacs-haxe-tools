3ff769030c5f57ecd590942b4642e56db95a4136
accidentalrebel <karlo@accidentalrebel.com>
2016-12-23 22:35:34 +0800

    Added more tests in haxe-tools-test.el.

--- tests/haxe-tools-test.el
+++ tests/haxe-tools-test.el
@@ -5,12 +5,50 @@
 (require 'ert)
 
 (ert-deftest t-haxe-tools-get-current-buffer-package-name()
+  ;; Initial test
+  (with-temp-buffer
+    (insert "package test.mypackage;\n")
+    (should (equal
+             (haxe-tools-get-current-buffer-package-name) "test.mypackage")))
+
+  ;; With a leading whitespace
+  (with-temp-buffer
+    (insert " package test.mypackage;\n")
+    (should (equal
+             (haxe-tools-get-current-buffer-package-name) "test.mypackage")))
+
+  ;; With a multiple levels
+  (with-temp-buffer
+    (insert "package test.test2.test3.mypackage;\n")
+    (should (equal
+             (haxe-tools-get-current-buffer-package-name) "test.test2.test3.mypackage")))
+
+  ;; With a package and import lines
   (with-temp-buffer
     (insert "package test.mypackage;\n")
     (insert "\n")
     (insert "import haxe.EnumTools;\n")
     (should (equal
-             (haxe-tools-get-current-buffer-package-name) "test.mypackage"))))
+             (haxe-tools-get-current-buffer-package-name) "test.mypackage")))
+
+  ;; With blank spaces before the package line
+  (with-temp-buffer
+    (insert "\n")
+    (insert "\n")
+    (insert "package test.mypackage;\n")
+    (insert "\n")
+    (insert "import haxe.EnumTools;\n")
+    (should (equal
+             (haxe-tools-get-current-buffer-package-name) "test.mypackage")))
+
+  ;; With the import statement before the package
+  (with-temp-buffer
+    (insert "import haxe.EnumTools;\n")
+    (insert "\n")
+    (insert "package test.mypackage;\n")
+    (should (equal
+             (haxe-tools-get-current-buffer-package-name) "test.mypackage")))
+  )
 
 (ert-deftest t-haxe-tools-put-current-buffer-package-name-to-clipboard()
   (with-temp-buffer
